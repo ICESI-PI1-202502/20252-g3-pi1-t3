ecd980766ace93a96e181140cddb444d9f9849f1
rashomon999 <luis.gluis.g.io.com@gmail.com>
2025-09-16 15:12:44 -0500

    resolution of mistake for a name an others...

--- project/BienestarUniversitario/settings.py
+++ project/BienestarUniversitario/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'universitaryWellbeing',
+    'Analytics_Reports',   
+
 ]
 
 MIDDLEWARE = [
@@ -113,10 +115,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- project/universitaryWellbeing/urls.py
+++ project/universitaryWellbeing/urls.py
@@ -1,19 +1,11 @@
+# universitaryWellbeing/urls.py
 from django.urls import path, include
 from . import views
-from . import views_analytics
 
 urlpatterns = [
-    
     path("", views.user_login, name="login"),
-    path('home/', views.home, name='home'),  # Esta línea debe existir
+    path("home/", views.home, name="home"),
     path("register/", views.register, name="register"),
     path("preferences/", views.preferences, name="preferences"),
-
-    # 🔹 rutas del módulo 5
-    path("analytics/", views_analytics.analytics_index, name="analytics_index"),
-    path("analytics/analisis/", views_analytics.analisis_comportamiento, name="analisis_comportamiento"),
-    path("analytics/comparaciones/", views_analytics.comparaciones, name="comparaciones"),
-    path("analytics/visualizacion/", views_analytics.visualizacion_exportacion, name="visualizacion"),
-    path("analytics/recomendaciones/", views_analytics.recomendaciones, name="recomendaciones"),
-    path("analytics/asistencia/", views_analytics.asistencia, name="asistencia"),
+    path("analytics-reports/", include("Analytics_Reports.urls", namespace="analytics_reports")),
 ]
