e2322089919d9a370aef59696012e29868696f33
rashomon999 <luis.gluis.g.io.com@gmail.com>
2025-09-16 10:39:50 -0500

    little mistake views

--- project/universitaryWellbeing/urls.py
+++ project/universitaryWellbeing/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from . import views
-from . import views_analytics
-
+ 
 urlpatterns = [
     
     path("", views.user_login, name="login"),
@@ -10,10 +9,10 @@ urlpatterns = [
     path("preferences/", views.preferences, name="preferences"),
 
     # 🔹 rutas del módulo 5
-    path("analytics/", views_analytics.analytics_index, name="analytics_index"),
-    path("analytics/analisis/", views_analytics.analisis_comportamiento, name="analisis_comportamiento"),
-    path("analytics/comparaciones/", views_analytics.comparaciones, name="comparaciones"),
-    path("analytics/visualizacion/", views_analytics.visualizacion_exportacion, name="visualizacion"),
-    path("analytics/recomendaciones/", views_analytics.recomendaciones, name="recomendaciones"),
-    path("analytics/asistencia/", views_analytics.asistencia, name="asistencia"),
+    path("analytics/", views.analytics_index, name="analytics_index"),
+    path("analytics/analisis/", views.analisis_comportamiento, name="analisis_comportamiento"),
+    path("analytics/comparaciones/", views.comparaciones, name="comparaciones"),
+    path("analytics/visualizacion/", views.visualizacion_exportacion, name="visualizacion"),
+    path("analytics/recomendaciones/", views.recomendaciones, name="recomendaciones"),
+    path("analytics/asistencia/", views.asistencia, name="asistencia"),
 ]
