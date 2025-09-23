550eecb91d09c9595baf678b198e8501e4390915
Davidジ <ISMAELBARRIONUEVO2006@GMAIL.COM>
2025-09-23 12:43:28 -0500

    module 1 updated

--- project/management_CADI/urls.py
+++ project/management_CADI/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.cadi_index, name="management_index"),
     path("crear-clasificacion/", views.create_Clasification, name="form_Clasifications"),
     path("crear-actividad/", views.create_Activities, name="form_Activities"),
+    path("añadir-horario/", views.add_schedule, name="form_Activities-2"),
 ]
