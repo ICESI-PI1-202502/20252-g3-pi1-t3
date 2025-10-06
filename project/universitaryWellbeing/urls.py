345327d2444a538c6c1e8c9b4b654cb535b6581a
David Chicué <davegeroficial@gmail.com>
2025-10-06 02:30:22 -0500

    fix: Resolve URL errors and update semaphore colors

--- project/universitaryWellbeing/urls.py
+++ project/universitaryWellbeing/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -11,4 +12,11 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("preferences/", views.preferences, name="preferences"),
     path("analytics-reports/", include("Analytics_Reports.urls", namespace="analytics_reports")),
+    path("tournaments/", include("tournaments.urls")),
+
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='auth/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='auth/password_reset_complete.html'), name='password_reset_complete'),
 ]
