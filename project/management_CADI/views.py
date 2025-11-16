dfb5a6c59109f56a38feef874ebb4bb9b11185ae
David Chicué <davegeroficial@gmail.com>
2025-11-16 15:43:01 -0500

    fix(news) ! : Huge fixes on the Admin management for news sucha as template management and responsiveness

--- project/management_CADI/views.py
+++ project/management_CADI/views.py
@@ -746,6 +746,13 @@ def create_Group_Activity(request, grupo_nombre, grupo_id):
 
     return render(request, "form_gruposActivi.html", {"grupo": grupo})
 
+@login_required
+@superuser_required
+def news_detail(request, slug, id):
+    # Fetch the news item by id and slug
+    noticia = get_object_or_404(Noticias, id=id, slug=slug)
+
+    return render(request, "news_detail.html", {"noticia": noticia})
 
 @login_required
 @superuser_required
@@ -767,7 +774,7 @@ def create_news(request):
         slug_real = slugify(noticia.titulo)
 
         # Redirigimos a una página de detalle o listado
-        return redirect("management_cadi:detalle_noticia", slug=slug_real, id=noticia.id)
+        return redirect("management_cadi:news_detail", slug=slug_real, id=noticia.id)
 
     return render(request, "form_news.html")
 
@@ -782,6 +789,8 @@ def edit_news(request, id):
         noticia.autor = request.POST.get("autor")
         noticia.descripcion = request.POST.get("descripcion")
 
+        noticia.autor = "Administración Bienestar Universitario"
+
         imagen_file = request.FILES.get("imagen")
         if imagen_file:
             noticia.imagen = imagen_file
@@ -790,7 +799,7 @@ def edit_news(request, id):
 
         slug_real = slugify(noticia.titulo)
 
-        return redirect("management_cadi:detalle_noticia", slug=slug_real, id=noticia.id)
+        return redirect("management_cadi:news:detail", slug=slug_real, id=noticia.id)
 
     return render(request, "edit_news.html", {"noticia": noticia})
 
@@ -801,25 +810,10 @@ def delete_news(request, id):
 
     if request.method == "POST":
         noticia.delete()
-        return redirect("management_cadi:listar_noticias")
+        return redirect("management_cadi:gestionar_noticias")
 
     return render(request, "delete_confirm.html", {"noticia": noticia})
 
-def news_detail(request, slug, id):
-    noticia = get_object_or_404(Noticias, id=id)
-    return render(request, "detail_news.html", {"noticia": noticia})
-
-
-def list_news(request):
-    noticias = Noticias.objects.all()  # Puedes agregar filtros si es necesario
-    paginator = Paginator(noticias, 10)  # Muestra 10 noticias por página
-
-    page = request.GET.get("page")
-    noticias_pag = paginator.get_page(page)
-
-    return render(request, "list_news.html", {
-        "noticias": noticias_pag
-    })
 
 @login_required
 @superuser_required
